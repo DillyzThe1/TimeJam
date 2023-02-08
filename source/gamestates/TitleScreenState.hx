b53b65346476b3d91b8457ef3bf466b97d8c1e26
DillyzThe1 <dyl1029an@gmail.com>
2023-02-08 00:04:34 -0600

    title screen work

--- source/gamestates/TitleScreenState.hx
+++ source/gamestates/TitleScreenState.hx
@@ -6,22 +6,35 @@ import flixel.FlxSprite;
 
 class TitleScreenState extends TJState
 {
-	public var logoSpr:FlxSprite;
+	var bg:FlxSprite;
+	var logoSpr:FlxSprite;
 
 	override public function create()
 	{
 		super.create();
 
-		logoSpr = new FlxSprite().loadGraphic(Paths.image('logo'));
+		bg = new FlxSprite().loadGraphic(Paths.image('bg'));
+		bg.screenCenter();
+		add(bg);
+
+		logoSpr = new FlxSprite().loadGraphic(Paths.image('glowy logo'));
 		logoSpr.screenCenter();
 		add(logoSpr);
+
+		logoSpr.scale.set(0.65, 0.65);
+		bg.antialiasing = logoSpr.antialiasing = true;
 	}
+
+	var totalElapsed:Float = 0;
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		totalElapsed += elapsed;
 
 		if (FlxG.sound.music == null)
 			FlxG.sound.playMusic(Paths.music("mainMenu"));
+
+		bg.angle = totalElapsed * 1.15;
 	}
 }
